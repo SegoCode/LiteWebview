298006205c51f2a16e32fadfc0253b62c5a8bccd
SegoCode <35817798+SegoCode@users.noreply.github.com>
2023-05-31 12:37:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow } = require('electron')
+const readline = require('readline')
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({
+    frame: false,
+    fullscreen: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  win.loadURL('https://github.com')
+
+  // Inject CSS to hide scrollbar
+  win.webContents.on('did-finish-load', () => {
+    win.webContents.insertCSS('body::-webkit-scrollbar { display: none; }')
+  })
+
+  // Full screen handling from the command line
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.on('line', (input) => {
+    console.log(`Received: ${input}`)
+  
+    if(input === 'fullscreen') {
+      win.setFullScreen(true)
+    }
+  })
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
