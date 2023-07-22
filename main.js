c125d8b9c058d7731707e11d408b44a4c87b2649
SegoCode <SegoCode@users.noreply.github.com>
2023-07-22 21:41:50 +0200

    Refactoring and fine-tuning of the app

--- main.js
+++ main.js
@@ -1,48 +1,37 @@
-const { app, BrowserWindow } = require('electron')
-const readline = require('readline')
+// Import required modules
+const { app, BrowserWindow } = require('electron') // Importing 'app' and 'BrowserWindow' from 'electron' module.
 
-let win
+let win // Define a variable to hold your application's main window.
 
+// Function to create a new application window
 function createWindow () {
+  // Instantiate a new BrowserWindow with specific options.
   win = new BrowserWindow({
-    frame: false,
-    fullscreen: true,
+    frame: false, // Creates a frameless window
+    fullscreen: true, // The window will be created in full-screen mode
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
+      nodeIntegration: true, // Allows use of Node.js APIs in the renderer process
+      contextIsolation: false // Running JavaScript in isolated context
     }
   })
 
   // Get the URL from the command line argument, if provided, else default to GitHub
-  const url = process.argv[2] || 'https://github.com/SegoCode'
+  let url = process.argv[2] || 'https://github.com/SegoCode';
+
+  // Load the specified URL into the BrowserWindow
   win.loadURL(url)
 
-  // Inject CSS to hide scrollbar
+  // After the page is fully loaded, insert custom CSS to hide the scrollbar
   win.webContents.on('did-finish-load', () => {
-    win.webContents.insertCSS('body::-webkit-scrollbar { display: none; }')
-  })
-
-  // Full screen handling from the command line
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-
-  rl.on('line', (input) => {
-    console.log(`Received: ${input}`)
-  
-    if(input === 'fullscreen') {
-      win.setFullScreen(true)
-    }
+    win.webContents.insertCSS('body::-webkit-scrollbar { display: none; }') // Injecting custom CSS
   })
 }
 
+// When Electron is fully initialized, create the application window
 app.whenReady().then(createWindow)
 
+// Event listener for when all application windows have been closed
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('activate', function () {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  // Quit the application
+  app.quit()
 })
